3ba8b17250d3aabc9906ace9b37a38d9e6bd95aa
Kugan Priyan <kugapriyanj97@gmail.com>
2023-08-30 16:14:26 +0530

    get token from headers

--- Server/Src/Middleware/VarifyUser.ts
+++ Server/Src/Middleware/VarifyUser.ts
@@ -7,13 +7,41 @@ import envValid from "../Utils/env-valid";
 
 
 export const varifyUser = async (req: Request, res: Response, next: NextFunction) => {
-    const token = await req.cookies.access_token;
-    console.log("check===========>", req.cookies);
+    // const token = await req.cookies.access_token;
 
-    if (!token) {
+
+    // if (!token) {
+    //     return res.status(401).json({ message: "Unauthorized" });
+    // }
+
+    // try {
+    //     const decoded = jwt.verify(token, envValid.JWT_SECRET) as JwtPayload;
+    //     const id = decoded.id;
+    //     const user = await User.findById(id);
+    //     if (!user) {
+    //         return res.status(401).json({ message: "Unauthorized" });
+    //     }
+    //     req.body.user = user;
+
+    //     next();
+    // } catch (error) {
+    //     if (error instanceof jwt.JsonWebTokenError) {
+    //         next(createError(401, "Invalid token"));
+    //     } else {
+    //         next(error);
+    //     }
+    // }
+
+    //using authorization header
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
         return res.status(401).json({ message: "Unauthorized" });
     }
 
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
     try {
         const decoded = jwt.verify(token, envValid.JWT_SECRET) as JwtPayload;
         const id = decoded.id;
@@ -31,4 +59,7 @@ export const varifyUser = async (req: Request, res: Response, next: NextFunction
             next(error);
         }
     }
+
+
+
 }
